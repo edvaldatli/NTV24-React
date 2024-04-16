b485399900d5eb0010e617f19d292a80ea38e4c2
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-04-16 01:14:18 +0000

    animations deleted

--- game-review-rewrite/game-review/app/components/modalOverlay.tsx
+++ game-review-rewrite/game-review/app/components/modalOverlay.tsx
@@ -30,7 +30,7 @@ const ModalOverlay: React.FC<PropsWithChildren<{}>> = ({ children }) => {
 
   return (
     <div
-      className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center transition translate-y-5 duration-200"
+      className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center"
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           closeModal();
